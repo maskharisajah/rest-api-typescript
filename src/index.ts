d0dd55700dea105d69b424558133282c3d7b9727
maskharisajah <maskharisajah@gmail.com>
2025-09-19 16:28:45 +0700

    feat: add logger & handle body request

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, { Application } from 'express';
 import { routes } from './routes';
+import { logger } from './utils/logger';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: Application = express();
 const port: number = 4000;
 
+//parse body request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// cors access handler
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
+
 routes(app);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => logger.info(`Server running on port ${port}`));
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { logger } from '../utils/logger';
 
 export const ProductRouter: Router = Router();
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Get product success');
   res.status(200).send({
     status: true,
     statusCode: 200,
@@ -12,5 +14,14 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
         price: 500000
       }
     ]
+  });
+});
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Create product success');
+  res.status(200).send({
+    status: true,
+    statusCode: 200,
+    data: req.body
   });
 });
