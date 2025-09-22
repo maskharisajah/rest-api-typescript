f4f86740196abe2df6f193e2114f131fb5ec0a1c
maskharisajah <maskharisajah@gmail.com>
2025-09-22 20:37:57 +0700

    feat: insert data to MongoDB

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,33 +1,38 @@
 import { Request, Response } from 'express';
 import { createProductValidation } from '../validations/product.validation';
 import { logger } from '../utils/logger';
-import { getProductFromDB } from '../services/product.service';
+import { addProductToDB, getProductFromDB } from '../services/product.service';
+import { v4 as uuidv4 } from 'uuid';
+import ProductType from '../types/product.type';
 
-interface ProductType {
-  product_id: string;
-  name: string;
-  price: number;
-  category: string;
-}
-
-export const createProduct = (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
+  req.body.product_id = uuidv4();
   const { error, value } = createProductValidation(req.body);
   if (error) {
     logger.error(`Err: product - create = ${error?.details[0]?.message}`);
     return res.status(422).send({
       status: false,
       statusCode: 422,
-      message: error?.details[0]?.message,
-      data: {}
+      message: error?.details[0]?.message
     });
   }
-  logger.info(`Create product success = ${JSON.stringify(value)}`);
-  return res.status(200).send({
-    status: true,
-    statusCode: 200,
-    message: 'Product created successfully',
-    data: value
-  });
+  try {
+    await addProductToDB(value);
+    logger.info(`Create product success = ${JSON.stringify(value)}`);
+    return res.status(201).send({
+      status: true,
+      statusCode: 201,
+      message: 'Product created successfully',
+      data: value
+    });
+  } catch (error) {
+    logger.error(`Err: product - create = ${error}`);
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error
+    });
+  }
 };
 
 export const getProduct = async (req: Request, res: Response) => {
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,10 @@
 import { logger } from '../utils/logger';
 import productModel from '../models/product.model';
+import ProductType from '../types/product.type';
+
+export const addProductToDB = async (payload: ProductType) => {
+  return await productModel.create(payload);
+};
 
 export const getProductFromDB = async () => {
   return await productModel
